457adadf88f356568b583cba651fbfee74f8d1b3
Acuros Kim <acuros@kaist.ac.kr>
2014-12-11 00:51:07 +0900

    python interpreter implemented

---
+++ defaults/python.py
@@ -0,0 +1,20 @@
+print 'Pykern Python Interpreter v0.1'
+lines = []
+while True:
+    indicator_str = '>>> ' if not lines else '...'
+    try:
+        line = raw_input(indicator_str)
+    except EOFError:
+        print
+        break
+    except KeyboardInterrupt:
+        lines = []
+        print
+        continue
+
+    lines.append(line)
+    if len(lines) == 1 and not lines[-1].endswith(':') or lines[-1] == '':
+        code = '\n'.join(lines)
+        exec compile(code, '<string>', 'exec') in dict()
+        lines = []
+
