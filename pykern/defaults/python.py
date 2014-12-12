e51960122b7d575bbd3bfefd579ed377dc92f862
Acuros Kim <acuros@kaist.ac.kr>
2014-12-12 12:05:58 +0900

    Refactored

--- pykern/defaults/python.py
+++ pykern/defaults/python.py
@@ -2,28 +2,39 @@ import readline
 import traceback
 
 print 'Pykern Python Interpreter v0.2'
-lines = []
-while True:
-    indicator_str = '>>> ' if not lines else '...'
-    try:
-        line = raw_input(indicator_str)
-    except EOFError:
-        print
-        break
-    except KeyboardInterrupt:
-        lines = []
-        print
-        continue
 
-    lines.append(line)
-    if len(lines) == 1 and not lines[-1].endswith(':') or lines[-1] == '':
-        code = '\n'.join(lines)
+
+def line_not_finished(line):
+    return not line.endswith(':')
+
+
+def main():
+    lines = []
+    while True:
+        indicator_str = '>>> ' if not lines else '... '
         try:
-            try:
-                print eval(code)
-            except SyntaxError:
-                exec compile(code, '<string>', 'exec')
-        except Exception, e:
-            traceback.print_exc()
+            line = raw_input(indicator_str)
+        except EOFError:
+            print
+            break
+        except KeyboardInterrupt:
+            lines = []
+            print
+            continue
 
-        lines = []
+        lines.append(line)
+        if len(lines) == 1 and line_not_finished(line) or line == '':
+            code = '\n'.join(lines)
+            try:
+                try:
+                    print eval(code)
+                except SyntaxError:
+                    exec compile(code, '<string>', 'exec')
+            except Exception, e:
+                traceback.print_exc()
+
+            lines = []
+
+
+if __name__ == '__main__':
+    main()
